89acd3c622fd4293aa97d9935a4065247afebf2d
Bhagya Sah <bhagyasah4u@gmail.com>
2018-10-16 14:23:15 +0545

    some extra in case statement

--- src/caseStatement.sh
+++ src/caseStatement.sh
@@ -1,13 +1,34 @@
 #! /bin/bash
 
-vehical=$1
+# example1...............
+# vehical=$1
 
-case $vehical in
-  "car" )
-  echo "Rent of $vehical is 100 dollar";;
-  "van" )
-  echo "Rent of $vehical is 80 dollar";;
-  * )
-  echo "unkown vehical";;
-  esac
+# case $vehical in
+#   "car" )
+#   echo "Rent of $vehical is 100 dollar";;
+#   "van" )
+#   echo "Rent of $vehical is 80 dollar";;
+#   * )
+#   echo "unkown vehical";;
+#   esac
+
+# .............................
+
+echo -e "enter some character: \c"
+read value
+
+case $value in
+    [a-z] )
+    echo "you enter sammal letter(a-z) : $value" ;;
+    [A-Z] )
+    echo "you Entered capital letter(A-Z) $value" ;;
+    ? )
+    echo "you entered special character : $value" ;;
+
+    * )
+    echo "unknown input"
+
+    esac
+
+
 
