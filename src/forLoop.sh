44c678c67a7ef54af0b6429cf8f1235d97a5fada
Bhagya Sah <bhagyasah4u@gmail.com>
2018-10-16 16:33:40 +0545

    more about for loop

--- src/forLoop.sh
+++ src/forLoop.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-for i in {10..2}
+# example no 1
+for i in {0..10..2}
 do
 echo $i
 (( ++i ))
 done
+
+# example no 2
+
+for command in ls pwd date
+do
+echo "---------------$command--------------------"
+  $command
+done
+
+#example no 3
+for item in *
+do
+if [ -d $item ]
+then
+echo $item
+fi
+done
+
